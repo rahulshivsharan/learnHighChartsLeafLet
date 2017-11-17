4a886956c558c8b45f0f00816ba36c8639400b33
rahulshivsharan <rahulshivsharan@gmail.com>
2017-11-17 19:05:39 +0530

    I Added angular-datatable and ng-i18next

--- app/i18/i18Initialise.js
+++ app/i18/i18Initialise.js
@@ -6,13 +6,13 @@ if(window.i18next){
 	
 	window.i18next.init({
 		debug: true,
-		lng: 'en', // If not given, i18n will detect the browser language.
-		fallbackLng: 'en', // Default is dev
+		lng: 'fr', // If not given, i18n will detect the browser language.
+		fallbackLng: 'fr', // Default is dev
 		//ns: ['englishLocale', 'frenchLocale'],
 		//defaultNS: 'englishLocale',
 		backend: {
 			loadPath: '/app/i18/locales/{{lng}}/{{ns}}.json'
-		},
+		},		
 		useCookie: false,
 		useLocalStorage: false
 	}, function (err, t) {
